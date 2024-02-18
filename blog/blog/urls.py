00621dfd1a995a2d7ea005a48d5518033a16bbb7
mmilton <murray.l.milton@outlook.com>
2024-02-18 13:48:26 +0100

    create the first part of the blog and 404 template

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlspattern = [
-    path("", views.starting_page),
-    path("posts", views.posts),
-    path("posts/<slug:slug>", views.post_detail), #/posts/my-first-post
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail, 
+        name="post-detail-page"), #/posts/my-first-post
 ]
